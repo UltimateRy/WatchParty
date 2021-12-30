3189a114fe5d9f6553111c58666c3eed1923a55c
UltimateRy <64440937+UltimateRy@users.noreply.github.com>
2021-12-30 02:09:39 +0000

    Installed the pusher libraries and added the Party Model

---
+++ database/migrations/2021_12_30_020418_create_parties_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePartiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('parties', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('host_id')->unsigned();
+            $table->string('movie_title');
+            $table->boolean('invite_only');
+            $table->timestamps();
+
+            $table->foreign('host_id')->references('id')->on('users')
+                ->onDelete('cascade')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('parties');
+    }
+}
