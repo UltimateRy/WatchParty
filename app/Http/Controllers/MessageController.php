da3755c822f0a1a001cdd0b56c3128a2db454541
UltimateRy <64440937+UltimateRy@users.noreply.github.com>
2022-01-16 00:53:34 +0000

    Removed spacing in MessageController

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -8,29 +8,16 @@ use Illuminate\Http\Request;
 
 class MessageController extends Controller
 {
-    //
-
     public function apiSend(Request $request)
     {
-        //
-
-        //return $request->input('user.username');
-
         $user = $request->input('user.id');
         $party = $request->input('party.id');
         $message = $request->input('message');
         
-        
         $sender = User::findOrFail($user);
-
         $message_wrap = $sender->username . " : " . $message;
 
-
         Broadcast(new NewMessage($user, $party, $message_wrap))->toOthers();
-
         return $message_wrap;
-
-        //return 1;
     }
-
 }
